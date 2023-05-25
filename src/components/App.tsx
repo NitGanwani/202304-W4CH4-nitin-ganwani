0dc1a09261f6d5a0c415c64b42b6e152d5ca993d
Nitin Ganwani <nitganwani@gmail.com>
2023-05-25 20:36:29 +0200

    Add context and custom hook components

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import { Display } from "./Display";
+import { Info } from "./Info";
+
+export function App() {
+  return (
+    <>
+      <Info></Info>
+      <main className="phone"></main>
+      <Display></Display>
+    </>
+  );
+}
--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,13 +1,7 @@
 export function Keyboard() {
   return (
-    <main className="phone">
-      <div className="keyboard-container">
-        <ol className="keyboard">
-          <li>
-            <button className="key big">delete</button>
-          </li>
-        </ol>
-      </div>
-    </main>
+    <div className="keyboard-container">
+      <ol className="keyboard"></ol>
+    </div>
   );
 }
